45dd20557aaa79b0ddd8f5104634a1c7b9038b58
Astrid Hecht <silentaviary@gmail.com>
2022-12-05 22:14:06 -0700

    Fix: update models with assoiciations and dependencies

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,2 +1,10 @@
 class Merchant < ApplicationRecord
+  validates :name, presence: true
+  validates :enabled, inclusion: [true, false]
+
+  has_many :items, dependent: :destroy
+  has_many :invoice_items, through: :items, dependent: :destroy
+  has_many :invoices, through: :invoice_items, dependent: :destroy
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices, dependent: :destroy
 end
