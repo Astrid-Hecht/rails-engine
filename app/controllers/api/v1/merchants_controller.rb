2c588495fcf890e877692963b5ff492f84a92745
Astrid Hecht <silentaviary@gmail.com>
2022-12-05 22:03:18 -0700

    feat: merchant show endpoint complete and passing tests and postman

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.all)
-  end 
+  end
+
+  def show
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant)
+  end
 end
