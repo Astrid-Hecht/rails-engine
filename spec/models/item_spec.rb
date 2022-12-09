b40d399ec7843dbf1daddca2f42b357ffd45e5a8
Astrid Hecht <silentaviary@gmail.com>
2022-12-09 11:33:13 -0700

    test: Finish model tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
+require 'shoulda-matchers'
+
 RSpec.describe Item, type: :model do
+  describe 'relationships' do
+    it { should belong_to(:merchant) }
+    it { should have_many :invoice_items }
+    it { should have_many(:invoices).through(:invoice_items) }
+  end
 
-#   describe 'relationships' do
-#     it { should belong_to(:merchant) }
-#     it { should have_many :invoice_items }
-#     it { should have_many(:invoices).through(:invoice_items) }
-#   end
-
-#   describe 'validations' do
-#     it { should validate_presence_of(:name) }
-#     it { should validate_presence_of(:description) }
-#     it { should validate_numericality_of(:unit_price) }
-#   end
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_numericality_of(:unit_price) }
+  end
 
   describe 'instance methods' do
     describe '#invoice_check' do
@@ -25,11 +26,11 @@ RSpec.describe Item, type: :model do
         invoice1 = create(:invoice, customer: cust, merchant_id: merch.id)
         invoice2 = create(:invoice, customer: cust, merchant_id: merch.id)
 
-        inv_item1 = create(:invoice_item, item: item1, invoice: invoice1)
+        _inv_item1 = create(:invoice_item, item: item1, invoice: invoice1)
 
-        inv_item2 = create(:invoice_item, item: item1, invoice: invoice2)
+        _inv_item2 = create(:invoice_item, item: item1, invoice: invoice2)
         inv_item3 = create(:invoice_item, item: item2, invoice: invoice2)
-        
+
         expect(Invoice.count).to eq(2)
 
         item1.invoice_check
@@ -54,58 +55,77 @@ RSpec.describe Item, type: :model do
       end
 
       it 'returns true if just name and name isnt empty' do
-        params = {'name': 'Bobs bobbers'}
+        params = { 'name': 'Bobs bobbers' }
         expect(Item.valid_search?(params)).to be true
       end
 
       it 'returns false if min price > max price' do
-        params = {'min_price': '15.00', 'max_price': '10.00'}
+        params = { 'min_price': '15.00', 'max_price': '10.00' }
         expect(Item.valid_search?(params)).to be false
       end
 
       it 'returns false if min price or max price are neg' do
-        params = {'min_price': '-15.00', 'max_price': '100.00'}
+        params = { 'min_price': '-15.00', 'max_price': '100.00' }
         expect(Item.valid_search?(params)).to be false
 
-        params = {'min_price': '15.00', 'max_price': '-100.00'}
+        params = { 'min_price': '15.00', 'max_price': '-100.00' }
         expect(Item.valid_search?(params)).to be false
 
-        params = {'min_price': '-15.00', 'max_price': '-100.00'}
+        params = { 'min_price': '-15.00', 'max_price': '-100.00' }
         expect(Item.valid_search?(params)).to be false
       end
 
       it 'returns true if min price and/or max price are present and arent empty' do
-        params = {'min_price': '15.00'}
+        params = { 'min_price': '15.00' }
         expect(Item.valid_search?(params)).to be true
 
-        params = {'max_price': '100.00'}
+        params = { 'max_price': '100.00' }
         expect(Item.valid_search?(params)).to be true
 
-        params = {'min_price': '15.00', 'max_price': '100.00'}
+        params = { 'min_price': '15.00', 'max_price': '100.00' }
         expect(Item.valid_search?(params)).to be true
       end
 
       it 'returns false if any key is empty' do
-        params = {'min_price': ''}
-        expect(Item.valid_search?(params)).to be false
-        
-        params = {'max_price': ''}
-        expect(Item.valid_search?(params)).to be false
-        
-        params = {'min_price': '', 'max_price': '100.00'}
+        params = { 'min_price': '' }
         expect(Item.valid_search?(params)).to be false
 
-        params = {'name': ''}
+        params = { 'max_price': '' }
+        expect(Item.valid_search?(params)).to be false
+
+        params = { 'min_price': '', 'max_price': '100.00' }
+        expect(Item.valid_search?(params)).to be false
+
+        params = { 'name': '' }
         expect(Item.valid_search?(params)).to be false
       end
     end
 
-    describe '#search_one' do
+    describe '#search_one & #price_logic' do
+      let!(:item2) { create(:item, name: 'Phill', unit_price: 1.50) }
+      let!(:item1) { create(:item, name: 'Bill', unit_price: 3.50) }
+      let!(:item3) { create(:item, name: 'Joll', unit_price: 5.55) }
 
-    end
+      it 'returns one object with a perfectly matching name' do
+        expect(Item.search_one({ 'name': 'Phill' })).to eq(item2)
+      end
 
-    describe '#price_logic' do
+      it 'returns first matching item alphabetically with a name fragment' do
+        expect(Item.search_one({ 'name': 'ill' })).to eq(item1)
+      end
 
+      it 'returns first matching item alphabetically with price parameters' do
+        expect(Item.search_one({ 'max_price': '3.00' })).to eq(item2)
+        expect(Item.search_one({ 'max_price': '30.00' })).to eq(item1)
+
+        expect(Item.search_one({ 'min_price': '4.00' })).to eq(item3)
+        expect(Item.search_one({ 'min_price': '3.00' })).to eq(item1)
+
+        expect(Item.search_one({ 'min_price': '4.00', 'max_price': '30.00' })).to eq(item3)
+        expect(Item.search_one({ 'min_price': '3.00', 'max_price': '30.00' })).to eq(item1)
+        expect(Item.search_one({ 'min_price': '1.00', 'max_price': '30.00' })).to eq(item1)
+        expect(Item.search_one({ 'min_price': '1.00', 'max_price': '3.00' })).to eq(item2)
+      end
     end
   end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,12 +1,40 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Merchant, type: :model do  
-#   describe 'relationships' do
-#     it { should have_many :items }
-#     it { should have_many :bulk_discounts }
-#   end
+RSpec.describe Merchant, type: :model do
+  describe 'relationships' do
+    it { should have_many :items }
+    it { should have_many(:invoice_items).through(:items) }
+    it { should have_many(:invoices).through(:invoice_items) }
+    it { should have_many(:customers).through(:invoices) }
+    it { should have_many(:transactions).through(:invoices) }
+  end
 
-#   describe 'validations' do
-#     it { should validate_presence_of(:name) }
-#   end
-# end
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe 'class methods' do
+    describe '#valid_search' do
+      it 'returns true if name present and name isnt empty' do
+        params = { 'name': 'Bobs bobbers' }
+        expect(Merchant.valid_search?(params)).to be true
+      end
+
+      it 'returns false if name isnt present' do
+        params = { 'key': 'value' }
+        expect(Merchant.valid_search?(params)).to be false
+      end
+
+
+      it 'returns false if name is empty' do
+        params = { 'Name': '' }
+        expect(Merchant.valid_search?(params)).to be false
+      end
+
+      it 'returns false if name isnt a string' do
+        params = { 'Name': 3523 }
+        expect(Merchant.valid_search?(params)).to be false
+      end
+    end
+  end
+end
